c42b32782d20a120468c38ec5c4a3ae7d1e8d8b7
sattu <shatakshisingh2005@gmail.com>
2026-02-25 20:18:29 +0530

    identity

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
 BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)))
-voice_sample = os.path.join(BASE_DIR, "Voice", "sattu_voice_sample.wav")
+voice_sample = os.path.join(BASE_DIR,  "sattu_voice_sample.wav")
